d13b87b18ea15232f2085a6fc2936bf9b316113f
Dorota Zelga <dorota.zelga@gmail.com>
2022-09-23 22:11:12 +0200

    introduction - first draft

--- src/components/intro/intro.component.tsx
+++ src/components/intro/intro.component.tsx
@@ -1,7 +1,30 @@
-import { Grid, useMultiStyleConfig } from '@chakra-ui/react';
+import { Box, Grid, Text, useMultiStyleConfig, Image } from '@chakra-ui/react';
+import avatar from './../../assets/avatar.jpeg';
 
 export const Intro = () => {
   const style = useMultiStyleConfig('intro', {});
 
-  return <Grid sx={style.root}>Intro</Grid>;
-};
+
+  // 1. annimated Hi -> Hi, Hello, Ahoj, Salut, Cześć, Hola
+
+
+  return (
+    <Grid sx={style.root}>
+      <Grid sx={style.content}>
+        <Box>
+          <Text fontSize='lg'>Hello, my name is </Text>
+          <Text fontSize='6xl'>Dorota Zelga</Text>
+          <Text fontSize='5xl'>I build web applications</Text>
+          <Text fontSize='lg' sx={style.description}>
+            I am a front-end developer spcializing in web applications.
+            My favourite stack is React, Typescript and GraphQL.
+            I like working on business solutions which brings the value.</Text>
+        </Box>
+        <Box sx={style.picture}>
+          <Image src={avatar} borderRadius='full' alt="Avatar" />
+        </Box>
+      </Grid>
+
+    </Grid>
+  )
+}
--- src/components/intro/intro.theme.ts
+++ src/components/intro/intro.theme.ts
@@ -1,10 +1,29 @@
 export const introTheme = {
   intro: {
-    parts: ['root'],
+    parts: ['root', 'content'],
     baseStyle: {
       root: {
-        background: 'red',
-        minHeight: '100vh',
+        minHeight: '100vh'
+      },
+      content: {
+        // border: '1px solid black',
+        width: 'fit-content',
+        minWidth: 'calc(55vw)',
+        maxWidth: '800px',
+        heigth: 'fit-content',
+        // minHeight: 'calc(50vh)',
+        margin: 'auto',
+        gridAutoFlow: 'row',
+        gridTemplateColumns: '7fr 3fr',
+        gap: 2
+      },
+      description: {
+        mt: '4',
+        maxWidth: '90%'
+      },
+      picture: {
+        minWidth: "100px",
+        margin: 'auto',
       }
     }
   }
