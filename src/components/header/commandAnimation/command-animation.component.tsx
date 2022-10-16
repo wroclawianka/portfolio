05a1d628592cdf84c0806189e4293d03d1f41233
Dorota Zelga <dorota.zelga@gmail.com>
2022-10-16 10:29:28 +0200

    commandAnimation - refactored

--- src/components/header/commandAnimation/command-animation.component.tsx
+++ src/components/header/commandAnimation/command-animation.component.tsx
@@ -1,63 +1,58 @@
 import { FunctionComponent, useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
 
+const titles = [
+    "software developer",
+    "npm i typescript",
+    "rm -rf",
+    "git push --force"
+];
+
+const duration = 120;
+
 type Props = {
     isStarted: boolean
-}
-
-type Element = {
-    title: string;
-    speed: number;
-}
+};
 
 export const CommandAnimation: FunctionComponent<Props> = ({ isStarted }) => {
-    const [command, setCommand] = useState('');
-    const [selected, setSelected] = useState(0);
-    const [position, setPosition] = useState(0);
-    const elements: Element[] = [
-        { title: 'software developer', speed: 150 },
-        { title: "npm i typescript", speed: 150 },
-        { title: "rm -rf", speed: 300 },
-        { title: 'git push --force', speed: 150 }
-    ];
+    const [text, setText] = useState<string>(''); // displayed text
+    const [titleIndex, setTitleIndex] = useState<number>(0); // index of the selected title in titles
+    const [charIndex, setCharIndex] = useState<number>(0); // index of the last character dusplayed in the title
 
     useEffect(() => {
         if (!isStarted) return
-        const { title, speed } = elements[selected];
-        const maxBlink = 6;
-        let blink = 0;
-        let _position = position;
-
+        const title = titles[titleIndex]; // selected title
+        const maxBlink = 6; // number of suffix blinking
+        let blinkIndex = 0, _charIndex = charIndex; // inital setup
 
         const interval = setInterval(() => {
-            // increase length of the command in every interval
-            if (_position < title.length) _position++
-            // showSuffix = !showSuffix
-            // setCommand([title.substring(0, _position), showSuffix ? "_" : ""].join(''))
-
-            setCommand(title.substring(0, _position))
+            // increase length of the text in every interval
+            if (_charIndex < title.length) _charIndex++
+            setText(title.substring(0, _charIndex))
 
             // go to next title or start from first after last one 
-            if (_position === title.length) {
-                if (blink < maxBlink - 1) {
-                    setCommand([title.substring(0, _position), !!(blink % 2) ? "_" : ""].join(''))
-                    blink++;
+            if (_charIndex === title.length) {
+                // blink the suffix after typing whole text
+                if (blinkIndex < maxBlink - 1) {
+                    setText([title.substring(0, _charIndex), blinkIndex % 2 ? "_" : ""].join(''))
+                    blinkIndex++;
 
                 } else {
-                    setPosition(0);
-                    selected === elements.length - 1
-                        ? setSelected(0)
-                        : setSelected(selected + 1);
+                    // go to next title after blinking effect
+                    setCharIndex(0);
+                    titleIndex === titles.length - 1
+                        ? setTitleIndex(0)
+                        : setTitleIndex(titleIndex + 1);
                 }
             }
-        }, speed);
+        }, duration);
 
         return () => {
-            setPosition(_position) // remember last position (freeze the content)
+            setCharIndex(_charIndex) // remember last charIndex on start
             clearInterval(interval)
         }
-    }, [selected, isStarted]);
+    }, [titleIndex, isStarted]);
 
 
-    return <Box color="grey" fontWeight="lighter">{command}</Box>
+    return <Box color="grey" fontWeight="lighter">{text}</Box>
 }
