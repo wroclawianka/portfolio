00197f658b2c37dc8d747f61d2c66acef51a5301
Dorota Zelga <dorota.zelga@gmail.com>
2022-10-16 10:09:45 +0200

    working animation

--- src/components/header/commandAnimation/command-animation.component.tsx
+++ src/components/header/commandAnimation/command-animation.component.tsx
@@ -1,45 +1,63 @@
-import { useEffect, useState } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
 
-export const CommandAnimation = () => {
+type Props = {
+    isStarted: boolean
+}
 
-    // 1. Add sections and scroll on click
-    // 2. dark.light more
-    // 2. Make if blink >_
-    // 3. Add changing titles on scroll
-    // Front-end developer
-    // Team player
-    // npm i typescript
-    // Agile enthusiast
-    // Amateur artist
-    // rm -rf
-    // coffee, coffee, coffee
+type Element = {
+    title: string;
+    speed: number;
+}
 
-    const titles = [
-        'software developer',
-        "npm i typescript",
-        "rm -rf",
+export const CommandAnimation: FunctionComponent<Props> = ({ isStarted }) => {
+    const [command, setCommand] = useState('');
+    const [selected, setSelected] = useState(0);
+    const [position, setPosition] = useState(0);
+    const elements: Element[] = [
+        { title: 'software developer', speed: 150 },
+        { title: "npm i typescript", speed: 150 },
+        { title: "rm -rf", speed: 300 },
+        { title: 'git push --force', speed: 150 }
     ];
 
-    // if reverse => faster
-
-
-    const [command, setCommand] = useState('');
-    const [index, setIndex] = useState(0)
-
     useEffect(() => {
-        let i = 0, reverse = false;
+        if (!isStarted) return
+        const { title, speed } = elements[selected];
+        const maxBlink = 6;
+        let blink = 0;
+        let _position = position;
+
+
         const interval = setInterval(() => {
-            if (i <= titles[index].length) reverse ? i-- : i++
-            setCommand(titles[index].substring(0, i))
-            if (i === 0 || i === titles[index].length) reverse = !reverse
-            if (i === 0) {
-                index === titles.length - 1 ? setIndex(0) : setIndex(index + 1)
+            // increase length of the command in every interval
+            if (_position < title.length) _position++
+            // showSuffix = !showSuffix
+            // setCommand([title.substring(0, _position), showSuffix ? "_" : ""].join(''))
+
+            setCommand(title.substring(0, _position))
+
+            // go to next title or start from first after last one 
+            if (_position === title.length) {
+                if (blink < maxBlink - 1) {
+                    setCommand([title.substring(0, _position), !!(blink % 2) ? "_" : ""].join(''))
+                    blink++;
+
+                } else {
+                    setPosition(0);
+                    selected === elements.length - 1
+                        ? setSelected(0)
+                        : setSelected(selected + 1);
+                }
             }
-        }, 250);
-        return () => clearInterval(interval);
-    }, [index]);
+        }, speed);
+
+        return () => {
+            setPosition(_position) // remember last position (freeze the content)
+            clearInterval(interval)
+        }
+    }, [selected, isStarted]);
 
 
-    return <Box color={"grey"}>{command}_</Box>
+    return <Box color="grey" fontWeight="lighter">{command}</Box>
 }
--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,4 +1,5 @@
 import { Grid, Heading, useMultiStyleConfig } from '@chakra-ui/react';
+import { useState } from 'react';
 // import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { CommandAnimation } from './commandAnimation';
@@ -6,13 +7,18 @@ import { CommandAnimation } from './commandAnimation';
 export const Header = () => {
   const style = useMultiStyleConfig('header', {});
   const { t } = useTranslation();
+  const [isAnimationStarted, setAnimation] = useState(false)
 
   return (
-    <Grid sx={style.root}>
+    <Grid
+      sx={style.root}
+      onMouseEnter={() => setAnimation(true)}
+      onMouseLeave={() => setAnimation(false)}
+    >
       <Heading sx={style.heading}>
         {t('header.title')}
         {'>'}
-        <CommandAnimation />
+        <CommandAnimation isStarted={isAnimationStarted} />
       </Heading>
       {/* <Switch sx={style.switch} size="lg" colorScheme="gray" /> */}
     </Grid>
